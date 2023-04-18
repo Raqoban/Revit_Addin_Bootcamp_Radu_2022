da8d76f08fa36752606d291197c9f24bc2fa4da5
Raqoban <64370591+Raqoban@users.noreply.github.com>
2023-04-18 12:22:51 -0700

    Add project files.

---
+++ Revit_Addin_Bootcamp_Radu_2022/Command1.cs
@@ -0,0 +1,39 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+#endregion
+
+namespace Revit_Addin_Bootcamp_Radu_2022
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // this is a variable for the Revit application
+            UIApplication uiapp = commandData.Application;
+
+            // this is a variable for the current Revit model
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            // Your code goes here
+            TaskDialog.Show("Test", "Updated to my add-in without Revit knowing!");
+
+            return Result.Succeeded;
+        }
+
+        public static String GetMethod()
+        {
+            var method = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
+            return method;
+        }
+    }
+}
